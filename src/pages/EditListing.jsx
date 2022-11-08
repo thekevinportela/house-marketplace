3d55642d2757a20945a8812c5582ff73f6dbe0ab
Kevin Portela <thekevinportela@gmail.com>
2022-11-08 15:46:03 -0500

    Google API key issue

--- src/pages/EditListing.jsx
+++ src/pages/EditListing.jsx
@@ -124,7 +124,7 @@ const EditListing = () => {
 
     if (geolocationEnabled) {
       const response = await fetch(
-        `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${API_KEY}`
       );
 
       const data = await response.json();
